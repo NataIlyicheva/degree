c5b613dde1271a97169585343ab228c6889ae4c4
Natali <n7483205@ya.ru>
2022-10-19 20:28:10 +0700

    Degree

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+double value = 0;
+value = Math.Pow(num1, num2);
+
+Console.WriteLine($"{num1} в степени {num2} = {value}");
